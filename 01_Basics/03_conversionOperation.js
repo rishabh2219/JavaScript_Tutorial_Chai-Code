d48a7d496858e7f19e53e117c39dc974d08b4308
Rishabh Ranjan <66666177+rishabh2219@users.noreply.github.com>
2023-11-23 06:51:20 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,6 +25,46 @@ let booleanIsloggedIn=Boolean(isloggedIn)
 
 let someNumber = 55
 let StringNumber=String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
 
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//************************ Operations *****************************
+
+let value=3
+let negvalue=-value
+
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Rishabh"
+
+let str3=str1+str2 
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4));
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1= num2 = num3 =2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
