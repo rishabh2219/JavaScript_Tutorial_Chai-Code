45de8547fc2431570dd77aad7a702713e7849d13
Rishabh Ranjan <66666177+rishabh2219@users.noreply.github.com>
2023-11-23 05:45:00 +0000

    Conversion of datatpes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score="Rishabh"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn="Rishabh"
+
+let booleanIsloggedIn=Boolean(isloggedIn)
+
+// console.log(typeof booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rishab" => true
+
+let someNumber = 55
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
