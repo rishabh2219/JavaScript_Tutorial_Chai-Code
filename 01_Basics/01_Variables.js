1d518420bdf128c51d75632a13893dea1ff0a8a6
Rishabh Ranjan <66666177+rishabh2219@users.noreply.github.com>
2023-11-23 04:52:19 +0000

    variables basics

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId=12345
+let accountEmail="Rishabh@google.com"
+var password="112345"
+accountCity="Bhopal"
+
+// accountId=2  // Not Allowed
+
+accountEmail="test@gmail.com"
+password="11223344"
+accountCity="Indore"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,password,accountCity])
+
+
+/*
+prefer nor to use var 
+because of issue in block scope and functional scope
+*/
