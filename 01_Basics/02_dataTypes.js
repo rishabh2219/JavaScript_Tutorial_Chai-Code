65d4271a4126583244bb40e79da06252a3632477
Rishabh Ranjan <66666177+rishabh2219@users.noreply.github.com>
2023-11-23 05:23:44 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all JS code as newer varsion
+
+//alert("6+6")  // We are using nodejs,not browser
+
+console.log(3
+     +
+     3) // code readability should be high
+
+console.log("Hello")
+
+
+let name="Rishabh"  
+let age=25          
+let isloggedIn = false
+let stat = null
+
+// numbers => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol 
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
